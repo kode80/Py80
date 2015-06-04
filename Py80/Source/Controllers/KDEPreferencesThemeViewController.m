31513d0a43cc777b7fe2b51d78bc5998fbee2439
Ben Hopkins <ben@kode80.com>
2015-06-04 15:56:31 -0400

    Implemented basic theme picking/editing. Added DefaultTheme.json.

--- Py80/Source/Controllers/KDEPreferencesThemeViewController.m
+++ Py80/Source/Controllers/KDEPreferencesThemeViewController.m
@@ -8,15 +8,51 @@
 
 #import "KDEPreferencesThemeViewController.h"
 
+#import "KDETheme.h"
+
+
 @interface KDEPreferencesThemeViewController ()
 
+@property (nonatomic, readwrite, strong) NSDictionary *themes;
+@property (nonatomic, readwrite, strong) NSArray *themeNames;
+@property (nonatomic, readwrite, strong) KDETheme *currentTheme;
+@property (nonatomic, readwrite, strong) NSArray *currentThemeItemNames;
+@property (nonatomic, readonly, strong) NSString *currentThemeItemName;
+
 @end
+
 
 @implementation KDEPreferencesThemeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSString *themePath = [[NSBundle mainBundle] pathForResource:@"DefaultTheme"
+                                                          ofType:@"json"];
+    KDETheme *theme = [[KDETheme alloc] initWithJSONAtPath:themePath];
+    self.themes = @{ @"Default" : theme };
+    self.themeNames = @[ @"Default" ];
+    self.currentTheme = theme;
 }
+
+#pragma mark - Accessors
+
+- (void) setCurrentTheme:(KDETheme *)currentTheme
+{
+    if( _currentTheme != currentTheme)
+    {
+        _currentTheme = currentTheme;
+        self.currentThemeItemNames = [currentTheme.itemNames sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+        [self reloadTableView:self.themeItemsTable];
+    }
+}
+
+- (NSString *) currentThemeItemName
+{
+    return self.currentThemeItemNames[ self.themeItemsTable.selectedRow];
+}
+
+#pragma mark - Actions
 
 - (IBAction) addTheme:(id)sender
 {
@@ -41,14 +77,20 @@
 
 - (IBAction) changeColor:(id)sender
 {
-    
+    [self.currentTheme setColor:[sender color]
+                    forItemName:self.currentThemeItemName];
+    [self reloadTableView:self.themeItemsTable];
 }
 
 - (IBAction) changeFont:(id)sender
 {
     NSFont *font = [NSFont systemFontOfSize:11.0f];
     font = [[NSFontManager sharedFontManager] convertFont:font];
-    self.fontButton.title = [NSString stringWithFormat:@"%@, %@pt", font.fontName, @(font.pointSize)];
+    
+    [self updateFontButtonWithFont:font];
+    [self.currentTheme setFont:font
+                   forItemName:self.currentThemeItemName];
+    [self reloadTableView:self.themeItemsTable];
 }
 
 - (NSUInteger) validModesForFontPanel:(NSFontPanel *)fontPanel
@@ -60,11 +102,33 @@
 
 - (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView
 {
-    return 8;
+    if( tableView == self.themesTable)
+    {
+        return self.themeNames.count;
+    }
+    else if( tableView == self.themeItemsTable)
+    {
+        return self.currentThemeItemNames.count;
+    }
+    return 0;
 }
 
 - (void) tableViewSelectionDidChange:(NSNotification *)notification
 {
+    NSTableView *tableView = notification.object;
+    NSInteger row = tableView.selectedRow;
+    
+    if( tableView == self.themesTable)
+    {
+        self.currentTheme = self.themes[ self.themeNames[ row]];
+    }
+    else if( tableView == self.themeItemsTable)
+    {
+        NSString *itemName = self.currentThemeItemName;
+        
+        [self updateFontButtonWithFont:[self.currentTheme fontForItemName:itemName]];
+        self.colorWell.color = [self.currentTheme colorForItemName:itemName];
+    }
 }
 
 #pragma mark - NSTableViewDelegate
@@ -76,9 +140,50 @@
     NSTableCellView *view = [tableView makeViewWithIdentifier:@"cell"
                                                         owner:self];
     
-    view.textField.stringValue = [tableColumn.identifier stringByAppendingFormat:@" Cell #%@", @(row)];
+    if( tableView == self.themesTable)
+    {
+        view.textField.stringValue = self.themeNames[ row];
+    }
+    else if( tableView == self.themeItemsTable)
+    {
+        NSString *itemName = self.currentThemeItemNames[ row];
+        NSFont *font = [self.currentTheme fontForItemName:itemName];
+        NSColor *color = [self.currentTheme colorForItemName:itemName];
+        
+        view.textField.stringValue = itemName;
+        if( font && color)
+        {
+            view.textField.textColor = color;
+        }
+    }
     
     return view;
+}
+
+#pragma mark - Private
+
+- (void) updateFontButtonWithFont:(NSFont *)font
+{
+    if( font == nil)
+    {
+        self.fontLabel.textColor = [NSColor disabledControlTextColor];
+        self.fontButton.title = @"None";
+        self.fontButton.enabled = NO;
+    }
+    else
+    {
+        self.fontLabel.textColor = [NSColor blackColor];
+        self.fontButton.title = [NSString stringWithFormat:@"%@, %@pt", font.fontName, @(font.pointSize)];
+        self.fontButton.enabled = YES;
+    }
+}
+
+- (void) reloadTableView:(NSTableView *)tableView
+{
+    NSInteger row = tableView.selectedRow;
+    [tableView reloadData];
+    [tableView selectRowIndexes:[NSIndexSet indexSetWithIndex:row]
+           byExtendingSelection:NO];
 }
 
 @end
