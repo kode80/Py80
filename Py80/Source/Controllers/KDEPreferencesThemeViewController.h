59a97c2fbf61acc12a5d8612e31e406663180aa0
Ben Hopkins <ben@kode80.com>
2015-06-04 14:52:20 -0400

    Updated theme preference UI. Implemented font picker.

--- Py80/Source/Controllers/KDEPreferencesThemeViewController.h
+++ Py80/Source/Controllers/KDEPreferencesThemeViewController.h
@@ -9,5 +9,24 @@
 #import <Cocoa/Cocoa.h>
 
 @interface KDEPreferencesThemeViewController : NSViewController
+<
+    NSTableViewDataSource,
+    NSTableViewDelegate
+>
+
+@property (nonatomic, readwrite, weak) IBOutlet NSTableView *themesTable;
+@property (nonatomic, readwrite, weak) IBOutlet NSTableView *themeItemsTable;
+@property (nonatomic, readwrite, weak) IBOutlet NSButton *addThemeButton;
+@property (nonatomic, readwrite, weak) IBOutlet NSButton *removeThemeButton;
+
+@property (nonatomic, readwrite, weak) IBOutlet NSTextField *fontLabel;
+@property (nonatomic, readwrite, weak) IBOutlet NSButton *fontButton;
+@property (nonatomic, readwrite, weak) IBOutlet NSColorWell *colorWell;
+
+- (IBAction) addTheme:(id)sender;
+- (IBAction) removeTheme:(id)sender;
+- (IBAction) pickFont:(id)sender;
+
+- (IBAction) changeColor:(id)sender;
 
 @end
--- Py80/Source/Controllers/KDEPreferencesThemeViewController.m
+++ Py80/Source/Controllers/KDEPreferencesThemeViewController.m
@@ -16,7 +16,69 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do view setup here.
+}
+
+- (IBAction) addTheme:(id)sender
+{
+    
+}
+
+- (IBAction) removeTheme:(id)sender
+{
+    
+}
+
+- (IBAction) pickFont:(id)sender
+{
+    NSFontManager *manager = [NSFontManager sharedFontManager];
+    
+    manager.target = self;
+    manager.action = @selector(changeFont:);
+    [manager setSelectedFont:[NSFont systemFontOfSize:11.0f]
+                  isMultiple:NO];
+    [manager orderFrontFontPanel:nil];
+}
+
+- (IBAction) changeColor:(id)sender
+{
+    
+}
+
+- (IBAction) changeFont:(id)sender
+{
+    NSFont *font = [NSFont systemFontOfSize:11.0f];
+    font = [[NSFontManager sharedFontManager] convertFont:font];
+    self.fontButton.title = [NSString stringWithFormat:@"%@, %@pt", font.fontName, @(font.pointSize)];
+}
+
+- (NSUInteger) validModesForFontPanel:(NSFontPanel *)fontPanel
+{
+    return NSFontPanelFaceModeMask | NSFontPanelSizeModeMask;
+}
+
+#pragma mark - NSTableViewDataSource
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView
+{
+    return 8;
+}
+
+- (void) tableViewSelectionDidChange:(NSNotification *)notification
+{
+}
+
+#pragma mark - NSTableViewDelegate
+
+- (NSView *) tableView:(NSTableView *)tableView
+    viewForTableColumn:(NSTableColumn *)tableColumn
+                   row:(NSInteger)row
+{
+    NSTableCellView *view = [tableView makeViewWithIdentifier:@"cell"
+                                                        owner:self];
+    
+    view.textField.stringValue = [tableColumn.identifier stringByAppendingFormat:@" Cell #%@", @(row)];
+    
+    return view;
 }
 
 @end
